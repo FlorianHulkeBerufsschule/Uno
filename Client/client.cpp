5291755d3a78487e9ecadc66d7b59ac983e242ac
JosuaBs <yunfan.xiao@bs-erlangen.de>
2024-02-29 12:21:08 +0100

    Button per code eingefügt

--- Client/client.cpp
+++ Client/client.cpp
@@ -24,9 +24,20 @@ void Client::onConnected()
     if (m_debug)
         qDebug() << "WebSocket connected";
     connect(&m_webSocket, &QWebSocket::textMessageReceived, this, &Client::onTextMessageReceived);
+}
+
+void Client::StartGame()
+{
+    m_webSocket.sendTextMessage(START_GAME);
+
+}
+
+void Client::Login(const QString name)
+{
 
     // Todo: The following must be executed by the frontend, not on init
     const QString name = "Name";
+    //const QString name = "Name";
     const QString payload = QJsonDocument(QJsonObject{{"name", name}}).toJson(QJsonDocument::Compact);
 
     m_webSocket.sendTextMessage(JOIN_QUEUE + ";" + payload);
--- Client/mainwindow.cpp
+++ Client/mainwindow.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "ui_StartScreen.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Create the button, make "this" the parent
+    m_button = new QPushButton("StartButton", this);
+    // set size and location of the button
+    m_button->setGeometry(QRect(QPoint(960, 520), QSize(200, 50)));
+
+    // Connect button signal to appropriate slot
+    connect(m_button, &QPushButton::released, this, &MainWindow::handleButton);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::handleButton()
+{
+    m_button->setText("Pissman");
 }
