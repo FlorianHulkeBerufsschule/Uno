1def75b420eb0dda42e753bef9bca60e5c788da8
JosuaBs <yunfan.xiao@bs-erlangen.de>
2024-03-21 10:28:57 +0100

    #48 Error wird angezeigt

--- Client/mainwindow.cpp
+++ Client/mainwindow.cpp
@@ -21,6 +21,7 @@ void MainWindow::generateClient(QUrl url, bool debug)
     m_client = new Client(url, debug);
     connect(m_client, &Client::showGameView, this, &MainWindow::showGameView);
     setCentralWidget(new QueueView(m_client));
+    connect(m_client, &Client::showError, this, &MainWindow::showError);
 }
 
 void MainWindow::showGameView(){
@@ -32,7 +33,11 @@ void MainWindow::showError(QJsonObject &payload)
     if(payload.contains("message") && payload["message"].isString())
     {
         QString message = payload["message"].toString();
-        // Todo display the error
+        QMessageBox msgBox;
+        msgBox.setWindowTitle("Error");
+        msgBox.setText(message);
+        msgBox.setStandardButtons(QMessageBox::Ok);
+        msgBox.exec();
     }
     else throw "Couldn't show error, 'message' is not contained";
 }
